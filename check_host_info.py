2b3afdc4b04f5c596581e0b736299efa11fc7a6c
fnstrange <101374780+fnstrange@users.noreply.github.com>
2022-04-11 13:49:24 -0400

    Código revisado - PEP8

--- check_host_info.py
+++ check_host_info.py
@@ -1,5 +1,6 @@
-
-import socket, urllib, platform, requests
+import socket
+import platform
+import requests
 from requests import get
 
 try:
@@ -7,34 +8,34 @@ try:
 except (requests.ConnectionError, requests.Timeout):
     print("Sin conexión a internet. Por favor verifique su conexión.")
 else:
-    if request == "<Response [200]>": #evalúa si es que el cliente tiene conexión a internet
-        
-        mysystem = platform.system() #obtener sistema operativo (windows, linux)
+    if request == "<Response [200]>":  # evalúa si es que el cliente tiene conexión a internet
+
+        mysystem = platform.system()  # obtener sistema operativo (windows, linux)
         if mysystem == "Linux":
-            def obtener_datos_linux(): 
-                try: 
-                    myhost = socket.gethostname()  #obtener nombre de host
-                    myip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #obtener direcció ip privada
+            def obtener_datos_linux():
+                try:
+                    myhost = socket.gethostname()  # obtener nombre de host
+                    myip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # obtener direcció ip privada
                     myip.connect(("8.8.8.8", 80))
-                    mypublic = get('https://api.ipify.org').content.decode('utf8') #obtener dirección ip pública
-                    print("[+] Hostname:    ",myhost) 
-                    print("[+] IP privada:  ",myip.getsockname()[0]) 
-                    print("[+] IP pública:  ",mypublic) 
-                except: 
-                    print("Error: no es posible obtener los datos") 
+                    mypublic = get('https://api.ipify.org').content.decode('utf8')  # obtener dirección ip pública
+                    print("[+] Hostname:    ", myhost)
+                    print("[+] IP privada:  ", myip.getsockname()[0])
+                    print("[+] IP pública:  ", mypublic)
+                except TypeError:
+                    print("Error: no es posible obtener los datos")
             obtener_datos_linux()
 
         elif mysystem == "Windows":
-            def obtener_datos_windows(): 
-                try: 
-                    myhost = socket.gethostname()  #obtener nombre de host
-                    myip = socket.gethostbyname(myhost) #obtener dirección ip privada
-                    mypublic = get('https://api.ipify.org').content.decode('utf8') #obtener dirección ip pública
-                    print("[+] Hostname:    ",myhost) 
-                    print("[+] IP privada:  ",myip) 
-                    print("[+] IP pública:  ",mypublic) 
-                except: 
-                    print("Error: no es posible obtener los datos") 
+            def obtener_datos_windows():
+                try:
+                    myhost = socket.gethostname()  # obtener nombre de host
+                    myip = socket.gethostbyname(myhost)  # obtener dirección ip privada
+                    mypublic = get('https://api.ipify.org').content.decode('utf8')  # obtener dirección ip pública
+                    print("[+] Hostname:    ", myhost)
+                    print("[+] IP privada:  ", myip)
+                    print("[+] IP pública:  ", mypublic)
+                except TypeError:
+                    print("Error: no es posible obtener los datos")
             obtener_datos_windows()
         else:
             pass
